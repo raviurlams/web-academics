b265c0c17bf77de7537cc8f81e390339b9f9ccc5
ravindra <rurlam@cisco.com>
2017-08-31 14:41:37 +0530

    initial setup

--- backend-app/routes.ts
+++ backend-app/routes.ts
@@ -1,24 +1,13 @@
 import * as express from 'express';
 
-import CatCtrl from './controllers/cat';
 import UserCtrl from './controllers/user';
-import Cat from './models/cat';
 import User from './models/user';
 
 export default function setRoutes(app) {
 
   const router = express.Router();
-
-  const catCtrl = new CatCtrl();
+  
   const userCtrl = new UserCtrl();
-
-  // Cats
-  router.route('/cats').get(catCtrl.getAll);
-  router.route('/cats/count').get(catCtrl.count);
-  router.route('/cat').post(catCtrl.insert);
-  router.route('/cat/:id').get(catCtrl.get);
-  router.route('/cat/:id').put(catCtrl.update);
-  router.route('/cat/:id').delete(catCtrl.delete);
 
   // Users
   router.route('/login').post(userCtrl.login);
--- frontend_app/app/app.module.ts
+++ frontend_app/app/app.module.ts
@@ -1,3 +1,4 @@
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
